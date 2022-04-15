458fa24f292a200b651e05bb7e01f2d92e5e9bb6
Guillaume <occuly.guillaume@gmail.com>
2022-04-15 13:45:20 +0200

    Uppy working

--- config/importmap.rb
+++ config/importmap.rb
@@ -5,6 +5,4 @@ pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
 pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
 pin_all_from "app/javascript/controllers", under: "controllers"
-pin "@uppy/core", to: "https://cdn.jsdelivr.net/npm/@uppy/core@2.1.8/lib/index.js"
-pin "@uppy/dashboard", to: "https://cdn.jsdelivr.net/npm/@uppy/dashboard@2.1.4/lib/index.js"
-pin "@uppy/tus", to: "https://cdn.jsdelivr.net/npm/@uppy/tus@2.2.2/lib/index.js"
+pin "uppy", to: "https://releases.transloadit.com/uppy/v2.9.3/uppy.min.js"
